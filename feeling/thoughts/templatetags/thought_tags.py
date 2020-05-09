0776f6818ead1d7643d4aca0291cda4c80b815aa
shakori999 <shakori999@gmail.com>
2020-05-09 08:34:29 +0430

    complete the chart graphs

--- feeling/thoughts/templatetags/thought_tags.py
+++ feeling/thoughts/templatetags/thought_tags.py
@@ -1,3 +1,6 @@
+import datetime
+import json
+from django.utils import timezone
 from django import template
 from ..form import ThoughtForm
 
@@ -8,3 +11,18 @@ register = template.Library()
 def thought_form():
     form = ThoughtForm()
     return {"form": form}
+
+
+@register.simple_tag(takes_context=True)
+def chart_data(context):
+    user = context["user"]
+    ten_days_ago = timezone.now() - datetime.timedelta(days=10)
+    thoughts = user.thoughts.filter(recorded_at__gte=ten_days_ago)
+    return json.dumps(
+        {
+            "labels": [
+                thought.recorded_at.strftime("%Y-%m-%d") for thought in thoughts
+            ],
+            "series": [[thought.condition for thought in thoughts]],
+        }
+    )
